0f4e4db64e492428b9a7a656fccec3701458a4bb
Chirag Gulati <chiraggulati098.com>
2025-04-19 23:40:59 +0530

    Added email_sync in app.py to run after gmail_service is created

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ from flask_cors import CORS
 from flask_pymongo import PyMongo
 import os
 import logging
-from utils.gmail_api import init_gmail_service
+from utils.gmail_api import init_gmail_service, get_user_email
+from utils.email_utils import sync_emails
 from routes.email_routes import register_routes
+
+# Setup logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 CORS(app)
@@ -19,10 +25,14 @@ with app.app_context():
 client_file = os.path.join(os.path.dirname(__file__), 'client_secret.json')
 gmail_service = init_gmail_service(client_file, api_name='gmail', api_version='v1', scopes=['https://mail.google.com/'])
 
-# Setup logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+# Sync emails after initializing Gmail service
+with app.app_context():
+    user_email = get_user_email(gmail_service)
+    if user_email:
+        logger.info(f"Starting initial email sync for {user_email}")
+        sync_emails(gmail_service, user_email, mongo)
+    else:
+        logger.error("Failed to get user email, skipping initial sync")
 
 # Register routes
 register_routes(app, gmail_service, mongo)
